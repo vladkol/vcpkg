df15374fa607e4885061fa7ba7b89c9b4a5a87cb
Robert Schumacher <roschuma@microsoft.com>
2017-07-06 02:53:51 -0700

    Small fixups after PR #1390

--- ports/openimageio/portfile.cmake
+++ ports/openimageio/portfile.cmake
@@ -1,15 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
@@ -55,9 +43,7 @@ vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-#clean
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-# Handle copyright of suitesparse and metis
 file(COPY ${CURRENT_BUILDTREES_DIR}/src/oiio-Release-1.7.15/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/openimageio)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/openimageio/LICENSE ${CURRENT_PACKAGES_DIR}/share/openimageio/copyright)
